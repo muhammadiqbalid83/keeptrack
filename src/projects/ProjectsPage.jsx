7c45e8678f5d694684a5278603aea8c8b851348f
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-07-01 10:24:02 +0700

    data

--- src/projects/ProjectsPage.jsx
+++ src/projects/ProjectsPage.jsx
@@ -1,3 +1,9 @@
+import { MOCK_PROJECTS } from "./MockProjects";
 export default function ProjectsPage() {
-  return <div>ProjectsPage</div>;
+  return (
+    <div>
+      <h1>Projects</h1>
+      <pre>{JSON.stringify(MOCK_PROJECTS, null, " ")}</pre>
+    </div>
+  );
 }
