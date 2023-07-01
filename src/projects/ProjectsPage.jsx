f5fa96d5ce4ac72eaedef69df407a3abf61b82d2
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-07-01 10:45:33 +0700

    Displaying Data

--- src/projects/ProjectsPage.jsx
+++ src/projects/ProjectsPage.jsx
@@ -1,9 +1,10 @@
 import { MOCK_PROJECTS } from "./MockProjects";
+import ProjectList from "./ProjectList";
 export default function ProjectsPage() {
   return (
     <div>
       <h1>Projects</h1>
-      <pre>{JSON.stringify(MOCK_PROJECTS, null, " ")}</pre>
+      <ProjectList projects={MOCK_PROJECTS} />
     </div>
   );
 }
